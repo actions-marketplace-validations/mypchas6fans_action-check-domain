de0f0b7680d718b5fcd87ad8957a1bdd489b38f4
Tony Tao <tony.tao@ingka.ikea.com>
2022-09-09 15:53:23 +0800

    use ISO format for output

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ try {
      */
     CheckCertificate(URL)
         .then(date => {
-            core.setOutput("ssl-expire-date", date.toString());
+            core.setOutput("ssl-expire-date", date.toISOString());
             core.setOutput("ssl-expire-days-left", Dates.countDays(date));
         })
         .catch(error => {
